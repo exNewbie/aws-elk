844561901c3de83c4255a042b0065511eb8b3d09
Trung Ly <trungldd@hotmail.com>
2018-03-30 18:31:04 +1000

    arrange files and folders

--- terraform/scripts/kibana.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-printf ${ProxyUsername}:`openssl passwd -apr1 ${ProxyPass}` >> /etc/nginx/conf.d/kibana.htpasswd
-# Remove the default location from nginx config
-sed -ri '/location \//,/.*\}/d' /etc/nginx/nginx.conf
-service nginx restart
